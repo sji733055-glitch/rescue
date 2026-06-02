d60fb72cb8682c2d081e594e6f29445efdd460b9
sji733055-glitch <sji733055@gmail.com>
2026-06-02 19:43:13 +0800

    feat(module/remote):加入获取通道值任务

--- modules/REMOTE/module_remote.c
+++ modules/REMOTE/module_remote.c
@@ -2,52 +2,52 @@
  * @Author: sji733055-glitch sji733055@gmail.com
  * @Date: 2026-05-27 19:55:55
  * @LastEditors: sji733055-glitch sji733055@gmail.com
- * @LastEditTime: 2026-05-29 14:04:49
+ * @LastEditTime: 2026-05-30 20:02:17
  * @FilePath: \rescue\modules\REMOTE\module_remote.c
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置:
+ * https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include "module_remote.h"
 #include "tx_api.h"
 #include "module_config.h"
 #include "bsp_def.h"
 
-#include"string.h"
-
+#include "string.h"
 
 #include "ps2.h"
 #define LOG_TAG "module_remote"
 #define LOG_LVL LOG_LVL_INFO
 #include "ulog_def.h"
 
-static Remote_data_t remote_data;
-static TX_THREAD remote_decode_task;
+static Remote_data_t              remote_data;
+static TX_THREAD                  remote_decode_task;
 APPS_STACK_SECTION static uint8_t remote_decode_stack[REMOTE_TASK_STACK_SIZE];
-static volatile bool              g_initialized;                               /* 模块初始化完成标志 */
+static volatile bool              g_initialized; /* 模块初始化完成标志 */
 
 static void remote_task_entry(ULONG arg)
 {
     (void)arg;
 
-    while (1) {
+    while (1)
+    {
         PS2_decode(&remote_data);
         tx_thread_sleep(2);
     }
-
 }
-
 
 void Module_Remote_init(void)
 {
-    if(g_initialized) return;
+    if (g_initialized) return;
 
-    memset(&remote_data,0,sizeof(Remote_data_t));
+    memset(&remote_data, 0, sizeof(Remote_data_t));
 
-    if(PS2_Device_Init()!=TX_SUCCESS)
+    if (PS2_Device_Init() != TX_SUCCESS)
     {
         LOG_E("module_ps2 init failed");
     }
 
-    UINT status =tx_thread_create(&remote_decode_task,"remote_task",remote_task_entry,0,remote_decode_stack,REMOTE_TASK_STACK_SIZE,REMOTE_TASK_PRIORITY,REMOTE_TASK_PRIORITY,TX_NO_TIME_SLICE,TX_AUTO_START);
+    UINT status = tx_thread_create(&remote_decode_task, "remote_task", remote_task_entry, 0, remote_decode_stack, REMOTE_TASK_STACK_SIZE,
+                                   REMOTE_TASK_PRIORITY, REMOTE_TASK_PRIORITY, TX_NO_TIME_SLICE, TX_AUTO_START);
     if (status != TX_SUCCESS)
     {
         LOG_E("Failed to create remote task");
@@ -57,4 +57,16 @@ void Module_Remote_init(void)
     LOG_I("remote_task init successfully!");
 }
 
+Remote_data_t *Module_Get_Data(void)
+{
+    if (!g_initialized) return NULL;
 
+    return &remote_data;
+}
+
+uint16_t Module_Get_Channels(uint8_t ch)
+{
+    if(!g_initialized || ch<1 || ch>3) return 0;
+
+    return remote_data.channels[ch-1];
+}
--- modules/REMOTE/module_remote.h
+++ modules/REMOTE/module_remote.h
@@ -1,9 +1,19 @@
+/*
+ * @Author: sji733055-glitch sji733055@gmail.com
+ * @Date: 2026-05-27 19:56:08
+ * @LastEditors: sji733055-glitch sji733055@gmail.com
+ * @LastEditTime: 2026-05-30 15:26:48
+ * @FilePath: \rescue\modules\REMOTE\module_remote.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #ifndef __MODULE_REMOTE_H__
 #define __MODULE_REMOTE_H__
 #include "stdint.h"
 typedef struct {
-    uint8_t channels[4];
+    uint8_t channels[2];
 }Remote_data_t;
 void Module_Remote_init(void);
+Remote_data_t *Module_Get_Data(void);
+uint16_t Module_Get_Channels(uint8_t ch);
 
 #endif /* __MODULE_REMOTE_H__ */
